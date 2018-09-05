6685e71a74524ffd0e2f217b2cf3614870c16c69
ShaneKing <github@azolla.org>
2018-09-05 22:31:20 +0800

    repath

--- offline.sh
+++ offline.sh
@@ -2,7 +2,7 @@
 
 git clone "https://github.com/ncjones/editorconfig-eclipse.git"
 
-cd ncjones/editorconfig-eclipse
+cd editorconfig-eclipse
 
 mvn clean install
 
@@ -10,10 +10,10 @@ cd ../../
 
 git clone "https://github.com/ShaneKing/editorconfig-eclipse-offline.git"
 
-cp -r ncjones/editorconfig-eclipse/editorconfig-eclipse-p2 ShaneKing/editorconfig-eclipse-offline/editorconfig-eclipse-p2
+cp -r editorconfig-eclipse/editorconfig-eclipse-p2 editorconfig-eclipse-offline/editorconfig-eclipse-p2
 
-git -C ./ShaneKing/editorconfig-eclipse-offline pull
-git -C ./ShaneKing/editorconfig-eclipse-offline add -A
-git -C ./ShaneKing/editorconfig-eclipse-offline commit -m "sync images at $(date +'%Y-%m-%d %H:%M')"
-git -C ./ShaneKing/editorconfig-eclipse-offline push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKing/editorconfig-eclipse-offline.git" mirror:mirror
+git -C ./editorconfig-eclipse-offline pull
+git -C ./editorconfig-eclipse-offline add -A
+git -C ./editorconfig-eclipse-offline commit -m "sync images at $(date +'%Y-%m-%d %H:%M')"
+git -C ./editorconfig-eclipse-offline push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKing/editorconfig-eclipse-offline.git" master:master
 

68ce6871466fe11738d16939c7676eb270dabef5
ShaneKing <github@azolla.org>
2018-09-05 22:50:59 +0800

    adjust

--- offline.sh
+++ offline.sh
@@ -10,12 +10,17 @@ mvn clean install
 
 cd ../../
 
+[[ -d editorconfig-eclipse-offline ]] && rm -rf ./editorconfig-eclipse-offline
+
 git clone "https://github.com/ShaneKing/editorconfig-eclipse-offline.git"
 
-cp -r editorconfig-eclipse/editorconfig-eclipse-p2 editorconfig-eclipse-offline/editorconfig-eclipse-p2
+cp -r ./editorconfig-eclipse/editorconfig-eclipse-p2 ./editorconfig-eclipse-offline/editorconfig-eclipse-p2
+
+ll ./editorconfig-eclipse-offline
+ll ./editorconfig-eclipse-offline/editorconfig-eclipse-p2
 
 git -C ./editorconfig-eclipse-offline pull
 git -C ./editorconfig-eclipse-offline add -A
-git -C ./editorconfig-eclipse-offline commit -m "sync images at $(date +'%Y-%m-%d %H:%M')"
-git -C ./editorconfig-eclipse-offline push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKing/editorconfig-eclipse-offline.git" master:master
+git -C ./editorconfig-eclipse-offline commit -m "sync at $(date +'%Y-%m-%d %H:%M')"
+git -C ./editorconfig-eclipse-offline push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKing/editorconfig-eclipse-offline.git"
 

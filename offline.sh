6ed8bd92d1e11f4fc299fe6c160895517fd99ac2
ShaneKing <github@azolla.org>
2018-09-05 23:11:09 +0800

    test

--- offline.sh
+++ offline.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pwd
+ls -alh $(dirname $0)
 git clone "https://github.com/ncjones/editorconfig-eclipse.git"
 
 cd editorconfig-eclipse
@@ -8,19 +10,26 @@ git submodule init && git submodule update
 
 mvn clean install
 
-cd ../../
+pwd
+ls -alh $(dirname $0)
+cd ../
 
 [[ -d editorconfig-eclipse-offline ]] && rm -rf ./editorconfig-eclipse-offline
 
+
+pwd
+ls -alh $(dirname $0)
 git clone "https://github.com/ShaneKing/editorconfig-eclipse-offline.git"
 
-cp -r ./editorconfig-eclipse/editorconfig-eclipse-p2 ./editorconfig-eclipse-offline/editorconfig-eclipse-p2
+cp -r editorconfig-eclipse/editorconfig-eclipse-p2 editorconfig-eclipse-offline/editorconfig-eclipse-p2
 
-ls -alh ./editorconfig-eclipse-offline
-ls -alh ./editorconfig-eclipse-offline/editorconfig-eclipse-p2
+pwd
+ls -alh $(dirname $0)
+ls -alh $(dirname $0)/editorconfig-eclipse-offline
+ls -alh $(dirname $0)/editorconfig-eclipse-offline/editorconfig-eclipse-p2
 
-git -C ./editorconfig-eclipse-offline pull
-git -C ./editorconfig-eclipse-offline add -A
-git -C ./editorconfig-eclipse-offline commit -m "sync at $(date +'%Y-%m-%d %H:%M')"
-git -C ./editorconfig-eclipse-offline push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKing/editorconfig-eclipse-offline.git"
+git -C $(dirname $0)/editorconfig-eclipse-offline pull
+git -C $(dirname $0)/editorconfig-eclipse-offline add -A
+git -C $(dirname $0)/editorconfig-eclipse-offline commit -m "sync at $(date +'%Y-%m-%d %H:%M')"
+git -C $(dirname $0)/editorconfig-eclipse-offline push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKing/editorconfig-eclipse-offline.git"
 

8cea0caa8d67bfbab4f0dfe11bb0f65cf929eea5
ShaneKing <github@azolla.org>
2018-09-06 00:20:46 +0800

    .

--- offline.sh
+++ offline.sh
@@ -15,7 +15,8 @@ cd editorconfig-eclipse-offline
 git pull
 #can not add target...
 #git add -A
-git add *
+git add .
+#git add -f .
 git add -u
 git commit -m "sync at $(date +'%Y-%m-%d %H:%M')"
 git push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKing/editorconfig-eclipse-offline.git" master:master
